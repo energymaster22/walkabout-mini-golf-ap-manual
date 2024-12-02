2e309db0275bed29f436f65dcd8fd3f292acaeaf
BuildTools <unconfigured@null.spigotmc.org>
2024-12-02 04:12:34 -0500

    Fixed incorrect import from "visibility" to "Visibility". Also removed default example option.

--- manual_walkabout_musicblox/hooks/Options.py
+++ manual_walkabout_musicblox/hooks/Options.py
@@ -1,5 +1,5 @@
 # Object classes from AP that represent different types of options that you can create
-from Options import FreeText, NumericOption, Toggle, DefaultOnToggle, Choice, TextChoice, Range, NamedRange, OptionList, visibility
+from Options import FreeText, NumericOption, Toggle, DefaultOnToggle, Choice, TextChoice, Range, NamedRange, OptionList, Visibility
 
 # These helper methods allow you to determine if an option has been set, or what its value is, for any player in the multiworld
 from ..Helpers import is_option_enabled, get_option_value
@@ -26,12 +26,6 @@ from ..Helpers import is_option_enabled, get_option_value
 # To add an option, use the before_options_defined hook below and something like this:
 #   options["total_characters_to_win_with"] = TotalCharactersToWinWith
 #
-class TotalCharactersToWinWith(Range):
-    """Instead of having to beat the game with all characters, you can limit locations to a subset of character victory locations."""
-    display_name = "Number of characters to beat the game with before victory"
-    range_start = 10
-    range_end = 50
-    default = 50
 
 class LinearLogic(DefaultOnToggle):
     """Disable to play with non-linear logic."""
