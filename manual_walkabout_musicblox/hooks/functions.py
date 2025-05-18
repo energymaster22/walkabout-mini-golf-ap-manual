c17b2692d3f7907c7bcdb485b3e00a123375cbf8
MusicBlox <robox243@gmail.com>
2025-05-18 15:15:56 -0700

    Properly embedded csv in apworld

--- manual_walkabout_musicblox/hooks/functions.py
+++ manual_walkabout_musicblox/hooks/functions.py
@@ -1,15 +1,19 @@
 import os
+from io import TextIOWrapper
+import zipfile
 import csv
 
 def get_courses():
 
     courses = []
-    data = os.path.abspath(os.path.join(os.getcwd(), "data\\meta\\courses.csv"))
+    print("Directory")
+    print(os.path.abspath(os.getcwd()))
 
-    with open(data) as courseFile:
-        reader = csv.reader(courseFile)
+    with zipfile.ZipFile('custom_worlds\\manual_walkabout_musicblox.apworld') as apworld:
+        with apworld.open('manual_walkabout_musicblox/data/meta/courses.csv', 'r') as courseFile:
+            reader = csv.reader(TextIOWrapper(courseFile, 'utf-8'))
 
-        for course in reader:
-            courses.append(course)
+            for course in reader:
+                courses.append(course)
 
     return courses
